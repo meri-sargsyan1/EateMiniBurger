ebe64e7ee594337e444529c040796a11ab285e19
meri-sargsyan1 <meri.sargsyan.08@mail.ru>
2025-06-24 17:32:05 +0400

    miniBurger

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.getElementById('sideMenu');
+    const overlay = document.getElementById('overlay');
+    menu.classList.toggle('open');
+    overlay.classList.toggle('show');
+}
